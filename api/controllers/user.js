7e4d1692cae75e4957dceffe288dd9b8eedc7c3a
Alexey Lyakhov <Alexey.V.Lyakhov@gmail.com>
2020-05-19 11:57:55 +0300

    CRUD user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,7 +3,31 @@ const db = require('../../utils/database')
 
 
 module.exports.findUserById = async (req, res) => {
+    try {
+    const user = await db.user.findOne({
+        where: {id: req.params.id}
+    })
+        res.status(200).json(user)
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+}
 
+module.exports.deleteUserById = async (req, res) => {
+    try {
+        let id = req.params.id
+
+        await db.user.destroy(({where: {id: id}}))
+        res.status(204).redirect('/')
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }
 
 module.exports.createUser = async (req, res) => {
@@ -38,6 +62,23 @@ module.exports.getAllUsers = async (req, res) => {
 module.exports.deleteAllUsers = async (req, res) => {
     try {
         await db.user.destroy({where: {}})
+        res.status(200).redirect('/')
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+}
+
+module.exports.changeUser = async (req, res) => {
+    try {
+        let user = await db.user.findOne({
+            where: {id: req.params.id}
+        })
+        user.firstName = req.body.firstName
+        user.lastName = req.body.lastName
+        await user.save()
         res.status(200).redirect('/')
     } catch (e) {
         console.log(e)
--- api/routes/user.js
+++ api/routes/user.js
@@ -5,7 +5,23 @@ const userController = require('../controllers/user')
 router.post('/user/create', userController.createUser)
 router.get('/user/all', userController.getAllUsers)
 router.get('/user/delete/all', userController.deleteAllUsers)
+router.get('/user/:id', userController.findUserById)
+router.get('/user/:id', userController.findUserById)
+router.post('/user/redactor/:id', userController.changeUser)
+router.get('/user/redactor/:id', (req, res) => {
+    const id = req.params.id
+    res.send(
+        `<form action="/api/user/redactor/${id}" method="post">
+        <label for="firstName">First name:</label>
+        <input id="firstName" type="text" name="firstName"><br>
+        <label for="lastName">Last name:</label>
+        <input id="lastName" type="text" name="lastName"><br>
+        <input type="submit" value="Change user"/>
+        </form>`
+    )
+})
 
+router.get('/user/delete/:id', userController.deleteUserById)
 
 
 module.exports = router
